ec30cc63b6fc92e411b2e0a045725a127598dbf9
SomGrap <ssomgrap@gmail.com>
2021-10-15 16:56:13 +0200

    Create maintenance.js

---
+++ discord_bot/commands/admin/maintenance.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: 'maintenance',
+    description: 'Enable ou disenable the maintenance mod.',
+    usage: ' <opt:args>',
+    category: 'admin',
+    async execute(message, args) {
+
+		
+
+    },
+};
