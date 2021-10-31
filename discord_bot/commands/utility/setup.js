ee7f3a4a466522d5a3f4bc4bb6b2d60dbecfff72
SomGrap <ssomgrap@gmail.com>
2021-10-31 16:47:30 +0100

    Create setup.js

---
+++ discord_bot/commands/utility/setup.js
@@ -0,0 +1,12 @@
+module.exports = {
+	name: "setup",
+	description: "",
+	usage: "<opt:pass>",
+	permissions: 'ADMINISTRATOR',
+	category: "utility",
+	async execute(message, args) {
+
+		client = message.client
+		
+	}
+};
