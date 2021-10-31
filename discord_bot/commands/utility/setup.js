a4a3bcaefd7e0735c99fd18cd71a0df9a70a34d0
SomGrap <ssomgrap@gmail.com>
2021-10-31 17:04:49 +0100

    Update setup.js

--- discord_bot/commands/utility/setup.js
+++ discord_bot/commands/utility/setup.js
@@ -1,12 +1,21 @@
 module.exports = {
 	name: "setup",
-	description: "",
+	description: "This command help you to setup TaskStreak on your server!",
 	usage: "<opt:pass>",
 	permissions: 'ADMINISTRATOR',
 	category: "utility",
 	async execute(message, args) {
 
 		client = message.client
+		guild = message.guild
 		
+		const { prefixes, defaultPrefixes, mainChannels, guildLanguages } = client;
+
+		const prefix = await prefixes.get(guild.id);
+		const defaultPrefix = await defaultPrefixes.get(guild.id);
+		const mainChannel = await mainChannels.get(guild.id);
+		const guildLanguage = await guildLanguages.get(guild.id);
+
+
 	}
 };
